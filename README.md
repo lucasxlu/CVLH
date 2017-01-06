6b48322e233964ac8147c7a9a95cc57f7e8598b8
EclipseXuLu <249048056@qq.com>
2017-01-06 15:18:10 +0800

    function improved

--- README.md
+++ README.md
@@ -8,5 +8,5 @@
    To make use of this repository, you should have several softwares installed on your machine.
    1. JDK1.8 or higer
    2. Maven support
-   3. In order to handle with captcha, you should have ![tessdata](https://github.com/tesseract-ocr/tessdata) directory in your project
+   3. In order to handle with captcha, you should have [tessdata](https://github.com/tesseract-ocr/tessdata) directory in your project
     
--- src/main/java/com/cvlh/spider/StudentInfoSpider.java
+++ src/main/java/com/cvlh/spider/StudentInfoSpider.java
@@ -1,10 +1,24 @@
 package com.cvlh.spider;
 
+import com.cvlh.util.Constant;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * a spider to crawl students' personal info
@@ -14,12 +28,40 @@ public class StudentInfoSpider {
 
     private static final String LOGIN_URL = "http://211.69.132.96/";
 
-    public void login() {
+    public void login(String username, String password) throws IOException {
+        HttpPost httpPost = new HttpPost(LOGIN_URL);
+        httpPost.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        httpPost.addHeader("Host", "211.69.132.96");
+        httpPost.addHeader("Origin", "http://211.69.132.96");
+        httpPost.addHeader("Referer", "http://211.69.132.96/");
+        httpPost.addHeader("Upgrade-Insecure-Requests", "1");
+        httpPost.addHeader("User-Agent", Constant.BROWSER_USER_AGENT);
+
+        List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+        formparams.add(new BasicNameValuePair("Flag", "Login"));
+        formparams.add(new BasicNameValuePair("username", username));
+        formparams.add(new BasicNameValuePair("password", password));
+        formparams.add(new BasicNameValuePair("ddlUserClass", "1"));
+        formparams.add(new BasicNameValuePair("code1", "1"));
+        formparams.add(new BasicNameValuePair("ctl00.x", "56"));
+        formparams.add(new BasicNameValuePair("ctl00.y", "15"));
+
+        UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+        httpPost.setEntity(urlEncodedFormEntity);
+
+        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+        CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost);
+        if (closeableHttpResponse.getStatusLine().getStatusCode() == 200) {
+            HttpEntity httpEntity = closeableHttpResponse.getEntity();
+            System.out.println(EntityUtils.toString(httpEntity));
+        }
 
     }
 
     /**
      * break captcha
+     *
      * @param captchaImgPath
      * @return
      * @throws TesseractException
