2515f629425dc8c42685fd861a6a70c51f4cc178
EclipseXuLu <249048056@qq.com>
2017-02-26 19:18:57 +0800

    fix a bug

---
+++ src/main/java/com/cvlh/util/FileUtil.java
@@ -0,0 +1,18 @@
+package com.cvlh.util;
+
+import java.util.List;
+
+/**
+ * Created by 29140 on 2017/2/26.
+ */
+public class FileUtil {
+
+    /**
+     * read the list and output the Excel file
+     *
+     * @param objectList
+     */
+    public static void outExcel(List<Object> objectList) {
+
+    }
+}
